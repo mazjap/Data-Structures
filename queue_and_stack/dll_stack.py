4ca107c0dba10660847c4a0e0029d9fe39c77490
Jordan Christensen <jordan.c4922@gmail.com>
2020-02-24 15:11:51 -0700

    Work on and finish dll queue and dll stack

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -4,15 +4,14 @@ from doubly_linked_list import DoublyLinkedList
 
 class Stack:
     def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        self.storage.add_to_tail(value)
 
     def pop(self):
-        pass
+        temp = self.storage.remove_from_tail()
+        return temp
 
     def len(self):
-        pass
+        return len(self.storage)
